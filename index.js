dec5f245a5e93a9ba84a6b25c9aa792469c16847
Daniel Vadil <dvadz@yahoo.com>
2022-01-03 18:25:38 -0500

    Set the full path to the /views folder for better access regarless of current working directory

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); //allows access to templates regardless of the current working
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
